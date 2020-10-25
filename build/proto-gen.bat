6fe69b96e472edce2ec3b34bb9c5ba8ca8e93628
Alexander Leitner <leitnersalex@gmail.com>
2020-10-25 14:36:37 -0400

    Fixed proto build script for windows

--- build/proto-gen.bat
+++ build/proto-gen.bat
@@ -1,1 +1,1 @@
-protoc --proto_path=.. --go_out=.. --go_opt=paths=source_relative --go-grpc_out=.. --go-grpc_opt=paths=source_relative ../internal/protobuf/*.proto
+protoc --proto_path=%~dp0\.. --go_out=%~dp0\..\internal\protobuf --go-grpc_out=%~dp0\..\internal\protobuf %~dp0\..\internal\protobuf\*.proto
