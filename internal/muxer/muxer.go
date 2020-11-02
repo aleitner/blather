bc4dafae2e8b4458d6420ca5c2a707a070d3bcb2
Alexander Leitner <leitnersalex@gmail.com>
2020-11-02 14:52:33 -0500

    Muxer can mix the streams

--- internal/muxer/muxer.go
+++ internal/muxer/muxer.go
@@ -3,6 +3,8 @@ package muxer
 import (
 	"sync"
 
+	"github.com/faiface/beep"
+
 	"github.com/aleitner/spacialPhone/internal/muxer/queue"
 
 	"github.com/aleitner/spacialPhone/internal/muxer/queue/strmr"
@@ -47,6 +49,52 @@ func (m *Muxer) Delete(id userid.ID) {
 	m.streamerQueues.Delete(id)
 }
 
-func (m *Muxer) Mux() {
+func (m *Muxer) Stream(samples [][2]float64) (n int, ok bool) {
+	var tmp [512][2]float64
 
+	for len(samples) > 0 {
+		toStream := len(tmp)
+		if toStream > len(samples) {
+			toStream = len(samples)
+		}
+
+		// clear the samples
+		for i := range samples[:toStream] {
+			samples[i] = [2]float64{}
+		}
+
+		snMax := 0 // max number of streamed samples in this iteration
+		m.streamerQueues.Range(func(key interface{}, value interface{}) bool {
+			st := value.(beep.Streamer)
+			// mix the stream
+			sn, sok := st.Stream(tmp[:toStream])
+			if sn > snMax {
+				snMax = sn
+			}
+			ok = ok || sok
+
+			for i := range tmp[:sn] {
+				samples[i][0] += tmp[i][0]
+				samples[i][1] += tmp[i][1]
+			}
+
+			return true
+		})
+
+		n += snMax
+		if snMax < len(tmp) {
+			break
+		}
+		samples = samples[snMax:]
+	}
+
+	// Stream silence
+	if len(samples) == 0 {
+		for i := range samples {
+			samples[i][0] = 0
+			samples[i][1] = 0
+		}
+	}
+
+	return n, ok
 }
